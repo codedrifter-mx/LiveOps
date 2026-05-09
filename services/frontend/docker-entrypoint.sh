456845c6bf7541f8feebe037ad80c26f5c49cfd9
codedrifter-mx <codedrifter@mx.com>
2026-05-09 13:24:22 -0600

    Revert "fix: nginx proxy /api to backend in production"
    This reverts commit 29d9c5658e36a7c5036fb459ad2e86bc90e8ebb3.

--- services/frontend/docker-entrypoint.sh
+++ services/frontend/docker-entrypoint.sh
@@ -2,5 +2,4 @@
 cat > /usr/share/nginx/html/config.js << EOF
 window.__BACKEND_URL__ = "${BACKEND_URL:-}";
 EOF
-sed -i "s|proxy_pass \${BACKEND_URL}/;|proxy_pass ${BACKEND_URL:-http://localhost:4000}/;|" /etc/nginx/conf.d/default.conf
 exec nginx -g "daemon off;"
